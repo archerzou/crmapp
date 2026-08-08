ad6224bc1755d624d4e4bcf3d8c107f29017b8a4
Zou Zhefeng <archer.zou84@gmail.com>
2026-08-09 00:48:58 +1200

    add common interface and models

---
+++ src/Application/Common/Interfaces/IApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using CrmApp.Domain.Entities;
+
+namespace CrmApp.Application.Common.Interfaces;
+
+public interface IApplicationDbContext
+{
+    DbSet<TodoList> TodoLists { get; }
+    DbSet<TodoItem> TodoItems { get; }
+
+    Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+}
