f1a4ff156cabdc84293b3d760c497cb0d2ea165b
Zou Zhefeng <archer.zou84@gmail.com>
2026-08-09 23:42:26 +1200

    complete todoitem application layer

---
+++ src/Application/TodoItems/Commands/CreateTodoItem/CreateTodoItemCommandValidator.cs
@@ -0,0 +1,11 @@
+﻿namespace CrmApp.Application.TodoItems.Commands.CreateTodoItem;
+
+public class CreateTodoItemCommandValidator : AbstractValidator<CreateTodoItemCommand>
+{
+    public CreateTodoItemCommandValidator()
+    {
+        RuleFor(v => v.Title)
+            .MaximumLength(200)
+            .NotEmpty();
+    }
+}
