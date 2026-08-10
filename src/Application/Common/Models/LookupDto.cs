3b4a8ef1c74be262fca882408c04523051b51a7f
Zou Zhefeng <archer.zou84@gmail.com>
2026-08-11 00:08:09 +1200

    feat: complete application layer

---
+++ src/Application/Common/Models/LookupDto.cs
@@ -0,0 +1,19 @@
+﻿using CrmApp.Domain.Entities;
+
+namespace CrmApp.Application.Common.Models;
+
+public class LookupDto
+{
+    public int Id { get; init; }
+
+    public string? Title { get; init; }
+
+    private class Mapping : Profile
+    {
+        public Mapping()
+        {
+            CreateMap<TodoList, LookupDto>();
+            CreateMap<TodoItem, LookupDto>();
+        }
+    }
+}
